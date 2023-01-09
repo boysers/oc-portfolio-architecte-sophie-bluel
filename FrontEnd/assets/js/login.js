98eb48335c034356d640c110a0bd35e55fa5b918
boysers <boyser2015@gmail.com>
2023-01-09 20:26:41 +0100

    feat: user authentification

---
+++ FrontEnd/assets/js/login.js
@@ -0,0 +1,53 @@
+if (isLogin()) {
+  location.href = './index.html'
+}
+
+document.querySelector("#loginForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  document.querySelector(".errorMessage")?.remove();
+
+  if (!this.reportValidity()) return;
+
+  const [email, password] = [
+    ...document.querySelectorAll('#loginForm input:not(input[type="submit"])'),
+  ].map((input) => input.value);
+
+  if (!email) {
+    this.insertAdjacentHTML(
+      "afterbegin",
+      `<p class="errorMessage">Saisissez votre adresse e-mail!</p>`
+    );
+    return;
+  } else if (!password) {
+    this.insertAdjacentHTML(
+      "afterbegin",
+      `<p class="errorMessage">Saisissez votre mot de passe!</p>`
+    );
+    return;
+  }
+
+  loadConfig().then(async (config) => {
+    const res = await fetch(`${config.api}/users/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    if (!res.ok) {
+      this.insertAdjacentHTML(
+        "afterbegin",
+        '<p class="errorMessage">Votre e-mail ou votre mot de passe est incorrect!</p>'
+      );
+      return;
+    }
+
+    const result = await res.json();
+
+    sessionStorage.setItem('user', JSON.stringify(result))
+
+    location.href = './index.html'
+  });
+});
