0a6be2670793fc2eb68aa963a2a71212bb3ffa96
boysers <boyser2015@gmail.com>
2023-01-09 18:32:25 +0100

    feat: add style filter-btn

--- FrontEnd/assets/js/home.js
+++ FrontEnd/assets/js/home.js
@@ -1,47 +1,55 @@
 loadConfig().then(async (config) => {
   const paths = ["works", "categories"];
 
-  const [listWork, listCategory] = await Promise.all(
-    paths.map((path) =>
-      fetch(`${config.api}/${path}`).then((res) => res.json())
-    )
-  );
-
   const gallery = document.querySelector(".gallery");
   const filter = document.querySelector(".filter");
 
-  filter.innerHTML += filterButton({ id: 0, name: "Tous" });
+  try {
+    const [listWork, listCategory] = await Promise.all(
+      paths.map((path) =>
+        fetch(`${config.api}/${path}`)
+          .then((res) => res.json())
+          .catch((err) => err)
+      )
+    );
 
-  addWorks(listWork, gallery);
+    if (listWork instanceof Error) throw listWork;
 
-  listCategory.forEach((category) => {
-    filter.innerHTML += filterButton(category);
-  });
+    filter.innerHTML += filterButton({ id: 0, name: "Tous" });
 
-  let preventIndexCategory = 0;
+    addWorks(listWork, gallery);
 
-  document.querySelectorAll(".filter-btn").forEach((btn) => {
-    btn.addEventListener("click", function () {
-      const btnCategoryId = this.getAttribute("data-category-id");
-
-      if (preventIndexCategory == btnCategoryId) return;
-
-      preventIndexCategory = btnCategoryId;
-
-      gallery.innerHTML = "";
-
-      if (btnCategoryId == 0) {
-        addWorks(listWork, gallery);
-        return;
-      }
-
-      const listWorkFiltered = listWork.filter((work) =>
-        work.categoryId == btnCategoryId ? 1 : 0
-      );
-
-      addWorks(listWorkFiltered, gallery);
+    listCategory.forEach((category) => {
+      filter.innerHTML += filterButton(category);
     });
-  });
+
+    let preventIndexCategory = 0;
+
+    document.querySelectorAll(".filter-btn").forEach((btn) => {
+      btn.addEventListener("click", function () {
+        const btnCategoryId = this.getAttribute("data-category-id");
+
+        if (preventIndexCategory == btnCategoryId) return;
+
+        preventIndexCategory = btnCategoryId;
+
+        gallery.innerHTML = "";
+
+        if (btnCategoryId == 0) {
+          addWorks(listWork, gallery);
+          return;
+        }
+
+        const listWorkFiltered = listWork.filter((work) =>
+          work.categoryId == btnCategoryId ? 1 : 0
+        );
+
+        addWorks(listWorkFiltered, gallery);
+      });
+    });
+  } catch (error) {
+    gallery.innerHTML = '<p id="errorProjectMessage">Projets indisponible!</p>'
+  }
 });
 
 function workCard(work) {
